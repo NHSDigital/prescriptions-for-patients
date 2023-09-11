2c82788929c6d774dcbb0181e87e8aa659e5fbef
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2023-09-11 14:20:54 +0000

    make tests work

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -6,13 +6,12 @@ for more ideas on how to test the authorization of your API.
 from os import getenv
 import pytest
 import requests
-
+import uuid
 
 @pytest.mark.smoketest
 def test_ping(nhsd_apim_proxy_url):
     resp = requests.get(f"{nhsd_apim_proxy_url}/_ping")
     assert resp.status_code == 200
-
 
 @pytest.mark.smoketest
 def test_wait_for_ping(nhsd_apim_proxy_url):
@@ -34,7 +33,6 @@ def test_wait_for_ping(nhsd_apim_proxy_url):
 
     assert deployed_commitId == getenv('SOURCE_COMMIT_ID')
 
-
 @pytest.mark.smoketest
 def test_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     resp = requests.get(
@@ -43,12 +41,12 @@ def test_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     assert resp.status_code == 200
     # Make some additional assertions about your status response here!
 
-
 @pytest.mark.smoketest
 def test_wait_for_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     retries = 0
     resp = requests.get(f"{nhsd_apim_proxy_url}/_status", headers=status_endpoint_auth_headers)
     deployed_commitId = resp.json().get("commitId")
+    deployment_status = resp.json().get("status")
 
     while (deployed_commitId != getenv('SOURCE_COMMIT_ID')
             and retries <= 30
@@ -66,27 +64,20 @@ def test_wait_for_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
         pytest.fail("version not found")
 
     assert deployed_commitId == getenv('SOURCE_COMMIT_ID')
+    assert deployment_status == 'pass'
 
-
-@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level0"})
-def test_app_level0(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
-    assert resp.status_code == 401  # unauthorized
-
-# @pytest.mark.nhsd_apim_authorization(
-#     {
-#         "access": "healthcare_worker",
-#         "level": "aal3",
-#         "login_form": {"username": "656005750104"},
-#     }
-# )
-
-# @pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
-# def test_app_level3(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-#     resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
-#     assert resp.status_code == 200
-
-
-# def test_cis2_aal3(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-#     resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
-#     assert resp.status_code == 200
+@pytest.mark.nhsd_apim_authorization(
+    {
+        "access": "patient",
+        "level": "P9",
+        "login_form": {"username": "9912003071"},
+        "authentication": "separate",
+    }
+)
+def test_authed_patient_can_access_bundle_endpoint(
+    nhsd_apim_proxy_url, nhsd_apim_auth_headers
+):
+    request_headers = nhsd_apim_auth_headers
+    request_headers['X-Request-Id'] = str(uuid.uuid4())
+    resp = requests.get(f"{nhsd_apim_proxy_url}/Bundle", headers=request_headers)
+    assert resp.status_code == 200
