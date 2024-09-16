e3b1411ac8f54e8594dcfb7ccbc66f584243d7c0
Sean Steberis <103416906+seansteberisal@users.noreply.github.com>
2024-09-16 11:20:01 +0100

    New: [AEA-4353] - Integrating PfP CI (#210)
    ## Summary
    
    Updated azure pipeline to include a step in the run_tests workflow for
    triggering the regression tests

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -3,6 +3,7 @@ See
 https://github.com/NHSDigital/pytest-nhsd-apim/blob/main/tests/test_examples.py
 for more ideas on how to test the authorization of your API.
 """
+
 from os import getenv
 import pytest
 import requests
@@ -21,9 +22,11 @@ def test_wait_for_ping(nhsd_apim_proxy_url):
     resp = requests.get(f"{nhsd_apim_proxy_url}/_ping")
     deployed_commitId = resp.json().get("commitId")
 
-    while (deployed_commitId != getenv('SOURCE_COMMIT_ID')
-            and retries <= 30
-            and resp.status_code == 200):
+    while (
+        deployed_commitId != getenv("SOURCE_COMMIT_ID")
+        and retries <= 30
+        and resp.status_code == 200
+    ):
         resp = requests.get(f"{nhsd_apim_proxy_url}/_ping")
         deployed_commitId = resp.json().get("commitId")
         retries += 1
@@ -33,7 +36,7 @@ def test_wait_for_ping(nhsd_apim_proxy_url):
     elif retries >= 30:
         pytest.fail("Timeout Error - max retries")
 
-    assert deployed_commitId == getenv('SOURCE_COMMIT_ID')
+    assert deployed_commitId == getenv("SOURCE_COMMIT_ID")
 
 
 @pytest.mark.smoketest
@@ -48,15 +51,21 @@ def test_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
 @pytest.mark.smoketest
 def test_wait_for_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     retries = 0
-    resp = requests.get(f"{nhsd_apim_proxy_url}/_status", headers=status_endpoint_auth_headers)
+    resp = requests.get(
+        f"{nhsd_apim_proxy_url}/_status", headers=status_endpoint_auth_headers
+    )
     deployed_commitId = resp.json().get("commitId")
     deployment_status = resp.json().get("status")
 
-    while (deployed_commitId != getenv('SOURCE_COMMIT_ID')
-            and retries <= 30
-            and resp.status_code == 200
-            and resp.json().get("version")):
-        resp = requests.get(f"{nhsd_apim_proxy_url}/_status", headers=status_endpoint_auth_headers)
+    while (
+        deployed_commitId != getenv("SOURCE_COMMIT_ID")
+        and retries <= 30
+        and resp.status_code == 200
+        and resp.json().get("version")
+    ):
+        resp = requests.get(
+            f"{nhsd_apim_proxy_url}/_status", headers=status_endpoint_auth_headers
+        )
         deployed_commitId = resp.json().get("commitId")
         retries += 1
 
@@ -67,8 +76,8 @@ def test_wait_for_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     elif not resp.json().get("version"):
         pytest.fail("version not found")
 
-    assert deployed_commitId == getenv('SOURCE_COMMIT_ID')
-    assert deployment_status == 'pass'
+    assert deployed_commitId == getenv("SOURCE_COMMIT_ID")
+    assert deployment_status == "pass"
 
 
 @pytest.mark.nhsd_apim_authorization(
@@ -83,15 +92,13 @@ def test_authed_patient_can_access_bundle_endpoint(
     nhsd_apim_proxy_url, nhsd_apim_auth_headers
 ):
     request_headers = nhsd_apim_auth_headers
-    request_headers['X-Request-Id'] = str(uuid.uuid4())
-    request_headers['X-Correlation-ID'] = str(uuid.uuid4())
+    request_headers["X-Request-Id"] = str(uuid.uuid4())
+    request_headers["X-Correlation-ID"] = str(uuid.uuid4())
     resp = requests.get(f"{nhsd_apim_proxy_url}/Bundle", headers=request_headers)
     assert resp.status_code == 200
 
 
 @pytest.mark.smoketest
 def test_un_authed_request_can_access_metadata_endpoint(nhsd_apim_proxy_url):
-    resp = requests.get(
-        f"{nhsd_apim_proxy_url}/metadata", headers=None
-    )
+    resp = requests.get(f"{nhsd_apim_proxy_url}/metadata", headers=None)
     assert resp.status_code == 200
