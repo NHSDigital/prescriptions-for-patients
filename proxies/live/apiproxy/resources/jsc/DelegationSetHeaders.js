6167b9d8b8f75affe068cb6e461849c91e9826d6
Tim Stephenson <231503406+tstephen-nhs@users.noreply.github.com>
2025-12-03 12:27:14 +0000

    fix: attempt to workaround missing Buffer

--- proxies/live/apiproxy/resources/jsc/DelegationSetHeaders.js
+++ proxies/live/apiproxy/resources/jsc/DelegationSetHeaders.js
@@ -9,8 +9,8 @@ var ctx = {
   subject: { nhs_number: subjectNhs },
   actor:   { nhs_number: actorSub }
 };
-var ctxJson = JSON.stringify(ctx);
-var ctxB64 = org.apache.commons.codec.binary.Base64.encodeBase64URLSafeString(
-  new java.lang.String(ctxJson).getBytes("UTF-8")
-);
-context.setVariable('nhsd.delegation.context_b64', ctxB64);
+// var ctxJson = JSON.stringify(ctx);
+// var ctxB64 = org.apache.commons.codec.binary.Base64.encodeBase64URLSafeString(
+//   new java.lang.String(ctxJson).getBytes("UTF-8")
+// );
+// context.setVariable('nhsd.delegation.context_b64', ctxB64);
